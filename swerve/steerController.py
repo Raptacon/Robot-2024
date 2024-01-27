815e766af4718f647271beba047180bd4b6570d3
Nathan Beasley <tuffinmuffin@gmail.com>
2024-01-27 14:13:36 -0700

    variable name fix

--- swerve/steerController.py
+++ swerve/steerController.py
@@ -9,21 +9,19 @@ class SteerController ():
     def __init__(self, swerveModule):
         self.module = swerveModule
         self.referenceAngle = 0.0
-        self.resetIteration = 0
-
 
     def getReferenceAngle(self):
         """Gets current set angle in radians"""
-        return self.referenceAngleRadians
+        return self.referenceAngle
 
 
     def setReferenceAngle(self, referenceAngle : float):
         '''https://github.com/SwerveDriveSpecialties/swerve-lib/blob/f6f4de65808d468ed01cc5ca39bf322383838fcd/src/main/java/com/swervedrivespecialties/swervelib/ctre/Falcon500SteerControllerFactoryBuilder.java#L181
            takes -180-180
         '''
+        self.referenceAngle = referenceAngle
         motor = self.module.getSteerMotor()
-        #motorEncoderVelocityCoefficient = self.module.getSteerSensorVelocityCoefficient() #removed due to code below being removed
-        currentAngle = self.module.getSteerAngle() # * motorEncoderPositionCoefficient
+        currentAngle = self.module.getSteerAngle()
 
         wpilib.SmartDashboard.putNumber(f"{self.module.steerId}Steer goal", referenceAngle)
         wpilib.SmartDashboard.putNumber(f"{self.module.steerId}Steer curr", currentAngle)
