7f37e4fa08243c24e0d015b16fd8a622086aae0a
Chris Hirsch <chris@kiatek.com>
2024-02-29 16:41:02 -0700

    Removed telemetry branch changes for subsystem/robotswerve.py

--- subsystem/swerveDriveTrain.py
+++ subsystem/swerveDriveTrain.py
@@ -11,121 +11,55 @@ import wpilib
 
 import ntcore
 
-
 class Drivetrain(commands2.SubsystemBase):
     kMaxVoltage = 12.0
-    kWheelBaseMeters = 0.7112  # front to back distance
-    kTrackBaseMeters = 0.6604  # left to right distance
-    # kMaxVelocityMPS = 4.14528
+    kWheelBaseMeters = 0.7112 # front to back distance
+    kTrackBaseMeters = 0.6604 # left to right distance
+    #kMaxVelocityMPS = 4.14528
     kMaxVelocityMPS = 1.0
-    kMaxAngularVelocityRadPS = kMaxVelocityMPS / math.hypot(
-        kWheelBaseMeters / 2.0, kTrackBaseMeters / 2.0
-    )
+    kMaxAngularVelocityRadPS = kMaxVelocityMPS / math.hypot(kWheelBaseMeters / 2.0, kTrackBaseMeters / 2.0)
 
     kModuleProps = [
-        {
-            "name": "frontLeft",
-            "channel": 50,
-            "encoderCal": 83.496,
-            "trackbase": kTrackBaseMeters / 2.0,
-            "wheelbase": kWheelBaseMeters / 2.0,
-            "inverted": False,
-        },
-        {
-            "name": "frontRight",
-            "channel": 53,
-            "encoderCal": 356.396,
-            "trackbase": -kTrackBaseMeters / 2.0,
-            "wheelbase": kWheelBaseMeters / 2.0,
-            "inverted": False,
-        },
-        {
-            "name": "rearLeft",
-            "channel": 56,
-            "encoderCal": 50.098,
-            "trackbase": kTrackBaseMeters / 2.0,
-            "wheelbase": -kWheelBaseMeters / 2.0,
-            "inverted": False,
-        },
-        {
-            "name": "rearRight",
-            "channel": 59,
-            "encoderCal": 69.785,
-            "trackbase": -kTrackBaseMeters / 2.0,
-            "wheelbase": -kWheelBaseMeters / 2.0,
-            "inverted": False,
-        },
+            {"name": "frontLeft", "channel": 50, "encoderCal": 83.496, "trackbase": kTrackBaseMeters/2.0, "wheelbase": kWheelBaseMeters/2.0, "inverted": False,  },
+            {"name": "frontRight", "channel": 53, "encoderCal": 356.396, "trackbase": -kTrackBaseMeters/2.0, "wheelbase": kWheelBaseMeters/2.0, "inverted": False },
+            {"name": "rearLeft", "channel": 56, "encoderCal": 50.098, "trackbase": kTrackBaseMeters/2.0, "wheelbase": -kWheelBaseMeters/2.0, "inverted": False },
+            {"name": "rearRight", "channel": 59, "encoderCal": 69.785, "trackbase": -kTrackBaseMeters/2.0, "wheelbase": -kWheelBaseMeters/2.0, "inverted": False  }
     ]
     kModulePropsNoCal = [
-        {
-            "name": "frontLeft",
-            "channel": 50,
-            "encoderCal": 0.0,
-            "trackbase": kTrackBaseMeters / 2.0,
-            "wheelbase": kWheelBaseMeters / 2.0,
-            "inverted": False,
-        },
-        {
-            "name": "frontRight",
-            "channel": 53,
-            "encoderCal": 0.0,
-            "trackbase": -kTrackBaseMeters / 2.0,
-            "wheelbase": kWheelBaseMeters / 2.0,
-            "inverted": True,
-        },
-        {
-            "name": "rearLeft",
-            "channel": 56,
-            "encoderCal": 0.0,
-            "trackbase": kTrackBaseMeters / 2.0,
-            "wheelbase": -kWheelBaseMeters / 2.0,
-            "inverted": False,
-        },
-        {
-            "name": "rearRight",
-            "channel": 59,
-            "encoderCal": 0.0,
-            "trackbase": -kTrackBaseMeters / 2.0,
-            "wheelbase": -kWheelBaseMeters / 2.0,
-            "inverted": True,
-        },
+            {"name": "frontLeft", "channel": 50, "encoderCal": 0.0, "trackbase": kTrackBaseMeters/2.0, "wheelbase": kWheelBaseMeters/2.0, "inverted": False },
+            {"name": "frontRight", "channel": 53, "encoderCal": 0.0, "trackbase": -kTrackBaseMeters/2.0, "wheelbase": kWheelBaseMeters/2.0, "inverted": True },
+            {"name": "rearLeft", "channel": 56, "encoderCal": 0.0, "trackbase": kTrackBaseMeters/2.0, "wheelbase": -kWheelBaseMeters/2.0, "inverted": False },
+            {"name": "rearRight", "channel": 59, "encoderCal": 0.0, "trackbase": -kTrackBaseMeters/2.0, "wheelbase": -kWheelBaseMeters/2.0, "inverted": True }
     ]
 
-    # 52 - -181.2
-    # 58 -  128.0
-    # 55 - -294.4
-    # 61 - -273.4
 
-    # new offests as of 6/30/2023 for swerve chasis
-    # 52 - 31.992
-    # 55 - 153.193
-    # 58 - -23.555
-    # 61 - 34.717
+#52 - -181.2
+#58 -  128.0
+#55 - -294.4
+#61 - -273.4
+
+#new offests as of 6/30/2023 for swerve chasis
+#52 - 31.992
+#55 - 153.193
+#58 - -23.555
+#61 - 34.717
     def __init__(self):
         super().__init__()
         self.swerveModules = list[SwerveModule]()
         self.datatable = ntcore.NetworkTableInstance.getDefault()
         self.table = self.datatable.getTable("Drivetrain")
         self.posTable = self.datatable.getTable("Robot position")
-        assert self.table
+        assert(self.table)
         for module in Drivetrain.kModuleProps:
             name = module["name"]
             subTable = self.table.getSubTable(name)
-            assert subTable
+            assert(subTable)
             wheelbase = module["wheelbase"]
             trackbase = module["trackbase"]
             channel = module["channel"]
             encoderCal = module["encoderCal"]
             inverted = module["inverted"]
-            self.swerveModules.append(
-                SwerveModule(
-                    (trackbase, wheelbase, name),
-                    channel,
-                    inverted,
-                    encoderCal,
-                    subTable,
-                )
-            )
+            self.swerveModules.append(SwerveModule((trackbase, wheelbase, name), channel, inverted, encoderCal, subTable))
 
         self.imu = navx.AHRS.create_spi()
 
@@ -142,15 +76,14 @@ class Drivetrain(commands2.SubsystemBase):
         self.moduleRotations = tuple(self.moduleRotations)
 
         self.headingOffset = 0
-        self.odometry = wpimath.kinematics.SwerveDrive4Odometry(
-            self.kinematics, self.getHeading(), self.moduleRotations
-        )
+        self.odometry = wpimath.kinematics.SwerveDrive4Odometry(self.kinematics, self.getHeading(), self.moduleRotations)
         self.pos = Pose2d()
         self.posX = 0
         self.posY = 0
         self.setFieldDriveRelative(True)
         self.ang = 0
         self.iteration = 0
+        
 
     def getHeading(self) -> Rotation2d:
         return Rotation2d.fromDegrees(self.imu.getFusedHeading() - self.headingOffset)
@@ -160,24 +93,25 @@ class Drivetrain(commands2.SubsystemBase):
         self.resetOdometry()
 
     def drive(self, xSpeed: float, ySpeed: float, rot: float, fieldRelative: bool):
-        # convert to proper units
-        # actually don't
-        rot = rot  # * 180.0
-        # print(f"drive: x {xSpeed}, y {ySpeed}, rot {rot}, field {fieldRelative}")
-        # rot = self.ang
-        # rot = rot * 360
-        # self.ang += 1.0
-        # rot = int(rot) % 180
-        # self.iteration += 1
-        # if(self.iteration % 100 == 0):
+        #convert to proper units
+        #actually don't
+        rot = rot# * 180.0
+        #print(f"drive: x {xSpeed}, y {ySpeed}, rot {rot}, field {fieldRelative}")
+        #rot = self.ang
+        #rot = rot * 360
+        #self.ang += 1.0
+        #rot = int(rot) % 180
+        #self.iteration += 1
+        #if(self.iteration % 100 == 0):
         #    print(f"drive: x {xSpeed}, y {ySpeed}, rot {rot}, field {fieldRelative}, speed {xSpeed * self.kMaxVoltage}")
 
-        # for mod in self.swerveModules:
+
+        #for mod in self.swerveModules:
         #    mod.set(xSpeed * self.kMaxVoltage, rot)
 
-        # return
-        # speed = max(abs(xSpeed), abs(ySpeed))
-        # ang = (math.degrees(math.atan2(ySpeed, xSpeed)) +90.0) %360.0
+        #return
+        #speed = max(abs(xSpeed), abs(ySpeed))
+        #ang = (math.degrees(math.atan2(ySpeed, xSpeed)) +90.0) %360.0
         # if(abs(xSpeed) < 0.8 and abs(ySpeed) < 0.8):
         #     pass
         #     print("pass")
@@ -190,13 +124,11 @@ class Drivetrain(commands2.SubsystemBase):
 
         chassisSpeeds = None
         if not fieldRelative:
-            # print("robot relative")
+            #print("robot relative")
             chassisSpeeds = wpimath.kinematics.ChassisSpeeds(ySpeed, -xSpeed, rot)
         else:
-            # print("field relative")
-            chassisSpeeds = wpimath.kinematics.ChassisSpeeds.fromFieldRelativeSpeeds(
-                ySpeed, -xSpeed, rot, self.getHeading()
-            )
+            #print("field relative")
+            chassisSpeeds = wpimath.kinematics.ChassisSpeeds.fromFieldRelativeSpeeds(ySpeed, -xSpeed, rot, self.getHeading())
 
         swerveModuleStates = self.kinematics.toSwerveModuleStates(chassisSpeeds)
         self.kinematics.desaturateWheelSpeeds(swerveModuleStates, self.kMaxVelocityMPS)
@@ -207,16 +139,12 @@ class Drivetrain(commands2.SubsystemBase):
         self.updateOdometry()
 
     def updateOdometry(self):
-        self.pos = self.odometry.update(
-            self.getHeading(),
-            [
-                self.swerveModules[1].getPosition(),
-                self.swerveModules[3].getPosition(),
-                self.swerveModules[0].getPosition(),
-                self.swerveModules[2].getPosition(),
-            ],
-        )
-
+        self.pos = self.odometry.update(self.getHeading(),
+                            [self.swerveModules[1].getPosition(),
+                            self.swerveModules[3].getPosition(),
+                            self.swerveModules[0].getPosition(),
+                            self.swerveModules[2].getPosition()])
+         
         self.posX = self.pos.Y()
         self.posY = self.pos.X()
 
@@ -228,25 +156,22 @@ class Drivetrain(commands2.SubsystemBase):
             self.posTable = self.datatable.getTable("Robot position")
 
     def resetOdometry(self):
-        self.odometry.resetPosition(
-            self.getHeading(),
-            [
-                self.swerveModules[1].getPosition(),
-                self.swerveModules[3].getPosition(),
-                self.swerveModules[0].getPosition(),
-                self.swerveModules[2].getPosition(),
-            ],
-            Pose2d(),
-        )
-
+        self.odometry.resetPosition(self.getHeading(),
+                            [self.swerveModules[1].getPosition(),
+                            self.swerveModules[3].getPosition(),
+                            self.swerveModules[0].getPosition(),
+                            self.swerveModules[2].getPosition()],
+                            Pose2d()
+                            )
+        
     def getPos(self) -> Pose2d:
         return self.pos
-
-    def disable(self, steer=True, drive=True):
+        
+    def disable(self, steer = True, drive = True):
         for m in self.swerveModules:
             m.disable(steer, drive)
 
-    def setSteer(self, angle: float):
+    def setSteer(self, angle : float):
         angle %= 360
         for m in self.swerveModules:
             m.setSteerAngle(angle)
@@ -275,6 +200,6 @@ class Drivetrain(commands2.SubsystemBase):
             m.setCal(enable)
 
     """Future improvment"""
-    # def periodic(self):
+    #def periodic(self):
     #    for m in self.swerveModules:
     #        m.periodic()
