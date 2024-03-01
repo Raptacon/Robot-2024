7f9ac00bbfecde6e1ba591de6a9b95a747e77926
DarthCooper <loganbarber1@outlook.com>
2024-03-01 10:04:38 -0700

    Made two basic move systems

--- auto.py
+++ auto.py
@@ -2,7 +2,7 @@ import commands2
 import commands2.cmd
 from commands.goToDist import GoToDist
 from subsystem.swerveDriveTrain import Drivetrain
-from commands.sparkyAuto import HandOff
+from commands.sparkyAuto import HandOff, GoToPos, GoForTime
 from subsystem.sparkyShooter import Shooter
 from subsystem.sparkyIntake import SparkyIntake
 import logging
@@ -19,10 +19,13 @@ class Autonomous(commands2.SequentialCommandGroup):
         )
 
 class SparkyShoot(commands2.SequentialCommandGroup):
-    def __init__(self, shooter : Shooter, intake : SparkyIntake) -> None:
+    def __init__(self, shooter : Shooter, intake : SparkyIntake, drive : Drivetrain) -> None:
         super().__init__()
         self.addCommands(
             commands2.PrintCommand("Running Shooter"),
             HandOff(shooter, intake, 2, 0.4),
-            commands2.PrintCommand("Finished Shooter"),
+            commands2.PrintCommand("Finished Shooter, started GoForTime"),
+            GoForTime(2, drive),
+            commands2.PrintCommand("Finished GoForTime, end auto"),
+            #GoToPos(Pose2d(0, 1, 0), drive),
         )
--- commands/sparkyAuto.py
+++ commands/sparkyAuto.py
@@ -2,6 +2,11 @@ import commands2
 from subsystem.sparkyIntake import SparkyIntake
 from subsystem.sparkyShooter import Shooter
 import time
+from subsystem.swerveDriveTrain import Drivetrain
+import wpimath.controller
+from wpimath.geometry import Pose2d, Transform2d
+import wpimath.units
+import math
 
 class HandOff(commands2.CommandBase):
     def __init__(self, shooter : Shooter, intake : SparkyIntake, maxTime : float, shooterWait : float) -> None:
@@ -28,3 +33,80 @@ class HandOff(commands2.CommandBase):
         self.intake.runIntake(0)
         self.shooter.runIntake(0)
         self.shooter.runShooters(0)
+
+class GoForTime(commands2.CommandBase):
+    def __init__(self, maxTime : float, drive : Drivetrain) -> None:
+        """Creates a new DriveDistance. This command will drive your your robot for a set period of time, should drive forward on the robots Y axis."""
+        super().__init__()
+        self.maxTime = maxTime
+        self.drive = drive
+        self.addRequirements(drive)
+
+    def initialize(self) -> None:
+        """Called when the command is initially scheduled."""
+        print("Init drive2distance")
+        self.drive.drive(0, 0, 0, True)
+        self.drive.resetHeading()
+        self.startingTime = time.time()
+
+    def execute(self) -> None:
+        """Called every time the scheduler runs while the command is scheduled."""
+        self.currentTime = time.time() - self.startingTime
+        self.drive.drive(0, 0.5, 0 ,True)
+
+    def end(self, interrupted: bool) -> None:
+        """Called once the command ends or is interrupted."""
+        self.drive.resetHeading()
+        self.drive.drive(0, 0, 0, True)
+
+    def isFinished(self) -> bool:
+        """Returns true when the command should end."""
+        # Compare distance travelled from start to desired distance
+        return self.currentTime >= self.maxTime
+
+class GoToPos(commands2.PIDSubsystem):
+    targetPos = Pose2d()
+    tolerance = Transform2d(0.5, 0.5, 0.1)
+    def __init__(self, targetPos: Pose2d, drive: Drivetrain) -> None:
+        """Creates a new DriveDistance. This command will drive your your robot to a designated cordinate.
+        targetPos: The X and Y pos that the bot will drive to(in meters)
+        drive:  The drivetrain subsystem on which this command will run
+        """
+        self.targetPos = targetPos
+        self.drive = drive
+        self.pid = wpimath.controller.PIDController(0.1, 0.0, 0.0)
+        self.pid.setTolerance(0.1, 0.1)
+        super().__init__(self.pid)
+
+    def initialize(self) -> None:
+        """Called when the command is initially scheduled."""
+        print("Init drive2distance")
+        self.drive.drive(0, 0, 0, True)
+        self.pid.reset()
+        self.drive.resetHeading()
+        self.startingPos = self.drive.getPos()
+
+    def execute(self) -> None:
+        """Called every time the scheduler runs while the command is scheduled."""
+        if(not self.drive.getPos()): return
+        self.dist = self.drive.getPos() - self.startingPos
+
+        self.totalOffset = self.targetPos - self.dist
+
+        self.speedX = self.pid.calculate(self.dist.X(), self.targetPos.X())
+        self.speedY = self.pid.calculate(self.dist.Y(), self.targetPos.Y())
+
+        print(f"pos : {self.drive.getPos().Y()} offsetX : {self.totalOffset.X()} offsetY : {self.totalOffset.Y()}")
+
+        self.drive.drive(self.speedX, self.speedY, 0 ,True)
+
+    def end(self, interrupted: bool) -> None:
+        """Called once the command ends or is interrupted."""
+        self.drive.drive(0, 0, 0, True)
+        self.drive.resetHeading()
+        self.pid.reset()
+
+    def isFinished(self) -> bool:
+        """Returns true when the command should end."""
+        # Compare distance travelled from start to desired distance
+        return self.totalOffset < self.tolerance
