50358fa617e969a9c7282d9ef6f3e577f754f668
raylmorris <123119109+raylmorris@users.noreply.github.com>
2024-03-16 17:13:42 +0000

    Changed field relative toggle to be held instead of pressed

--- commands/togglefielddrive.py
+++ commands/togglefielddrive.py
@@ -1,13 +1,17 @@
 import commands2
+import typing
 from subsystem.swerveDriveTrain import Drivetrain
 
 class ToggleFieldDrive(commands2.InstantCommand):
-    def __init__(self, driveTrain: Drivetrain) -> None:
+    def __init__(self, 
+                 driveTrain: Drivetrain, 
+                 robotRelative: typing.Callable[[],bool]) -> None:
         super().__init__()
         self.driveTrain = driveTrain
+        self.robotRelative = robotRelative
         self.addRequirements(driveTrain)
 
     def execute(self) -> None:
-        print(f"Toggling field relative from {self.driveTrain.getFieldDriveRelative()} to {not self.driveTrain.getFieldDriveRelative()}")
-        self.driveTrain.setFieldDriveRelative(not self.driveTrain.getFieldDriveRelative())
+        print(f"Toggling field relative from {self.driveTrain.getFieldDriveRelative()} to {not self.robotRelative()}")
+        self.driveTrain.setFieldDriveRelative(not self.robotRelative())
 
