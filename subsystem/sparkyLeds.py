eaf223e1ce41c22964051b16ec7e5cd284686768
Nathan Beasley <tuffinmuffin@gmail.com>
2024-03-21 09:22:31 -0600

    add 2 new LED strips (#80)
    * added new LED strips
    
    * bug fixes for leds
    
    * name updates

--- subsystem/sparkyLeds.py
+++ subsystem/sparkyLeds.py
@@ -3,27 +3,33 @@ import wpilib
 from utils.leds import Strip
 
 class Leds(commands2.Subsystem):
-    kLEDBuffer = 18 #one side temp
+    kLEDBuffer = 36 #one side temp
     def __init__(self):
         self.leds = wpilib.AddressableLED(0)
         self.ledData = [wpilib.AddressableLED.LEDData() for _ in range(self.kLEDBuffer)]
         self.currentHue = 243
         self.currentBright = 0
-        #9 LED right 9 LED right
+        #9 LED frontRight 9 LED frontRight
         self.strips = {}
-        #Right - first 9 in order
-        self.strips["right"]= Strip(self.ledData[0:9])
-        #Left 2nd set of 9 reverse order
-        self.strips["left"] =  Strip(self.ledData[9:18][::-1])
-        self.strips["right"].setTeamColor()
-        self.strips["left"].setTeamColor()
+        #Right Front - first 9 in order
+        self.strips["frontRight"]= Strip(self.ledData[0:9], "frontRight")
+        #Right Read- 2nd 9 in reverse order
+        self.strips["rearRight"]= Strip(self.ledData[9:18][::-1], "rearRight")
+        #Left 3rd set of 9 in order
+        self.strips["rearLeft"] = Strip(self.ledData[18:27], "rearLeft")
+        #Left 4th set of 9 reverse order
+        self.strips["frontLeft"] = Strip(self.ledData[27:36][::-1], "frontLeft")
+        self.strips["frontRight"].setTeamColor()
+        self.strips["frontLeft"].setTeamColor()
+        self.strips["rearLeft"].setRainbowHue()
+        self.strips["rearRight"].setRainbowHue()
 
         #Confiugure the LED strip
         self.leds.setLength(self.kLEDBuffer)
         self.leds.setData(self.ledData)
         self.leds.start()
 
-        
+
     def getStrip(self, name: str) -> Strip:
         if name in self.strips:
             return self.strips[name]
