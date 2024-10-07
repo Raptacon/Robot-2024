99700cc728eeca1da5c07425ccd35464ff284106
DanteSGarrett <dante.s.garrett@gmail.com>
2024-10-07 16:46:32 -0600

    finished auto driving prototype

--- auto.py
+++ auto.py
@@ -19,6 +19,10 @@ class SparkyShoot(commands2.SequentialCommandGroup):
             AutoDrive(.01, 0, -.1, 0.2, True, drive), #align wheels
             AutoDrive(1.0, 0, 0.0, 0.0, True, drive), #safe the drive train until we are stable
             commands2.PrintCommand("AutoDrive Align finished"),
-            AutoDrive(1.2, 0, -0.2, 0.0, True, drive), #drive forward about 6 ft
-            commands2.PrintCommand("AutoDrive move finished"),
+            AutoDrive(1.2, 0, -0.2, 0.0, True, drive), #drive forward about 6 ft, arm going down during this
+            commands2.PrintCommand("AutoDrive first move finished"),
+            AutoDrive(0.2, 0, -0.2, 0, True, drive), #drive foward about 1 ft, may need to change values, arm sucking peice
+            commands2.PrintCommand("AutoDrive second move finished"),
+            AutoDrive(1.4, 0, 0.2, 0, True, drive), #move back to the scoring area, may need to change values, arm going up during this
+            commands2.PrintCommand("AutoDrive final move finnished"),
         )
